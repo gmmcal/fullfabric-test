7fb368ca6276bc92ad35dd44b895a014d6b7c1fa
Leonardo Aretakis <leoaretakis@gmail.com>
2017-06-19 15:53:22 +0100

    the end

--- lib/game.rb
+++ lib/game.rb
@@ -2,7 +2,8 @@ class Game
   attr_reader :player_1,
     :player_2,
     :attack_die,
-    :defense_die
+    :defense_die,
+    :winner
 
   def initialize( player_1:,
                   player_2:,
@@ -13,5 +14,32 @@ class Game
     @player_2 = Player.new(player_2, life_points)
     @attack_die = Die.new(attack_die_sides)
     @defense_die = Die.new(defense_die_sides)
+  end
+
+  def play!
+    attacker = @player_1
+    defender = @player_2
+
+    while @winner.nil? do
+
+      round = Round.new(
+        id: 1,
+        attacker: attacker,
+        defender: defender,
+        attack_die: @attack_die,
+        defense_die: @defense_die
+      )
+
+      round.fight
+
+      if defender.life_points <= 0
+        @winner = attacker
+      else
+        temp = attacker
+        attacker = defender
+        defender = temp
+      end
+
+    end
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -13,4 +13,35 @@ describe Game do
   it { expect(subject.player_2).to_not be_nil }
   it { expect(subject.attack_die).to_not be_nil }
   it { expect(subject.defense_die).to_not be_nil }
+
+  context '.play' do
+
+    it 'plays a round which kills player_2' do
+      allow(subject.attack_die).to receive(:roll) { 21 }
+      allow(subject.defense_die).to receive(:roll) { 1 }
+
+      subject.play!
+
+      expect(subject.winner).to eq(subject.player_1)
+    end
+
+    it 'plays 2 rounds which kills player_1' do
+      allow(subject.attack_die).to receive(:roll).and_return(11, 21)
+      allow(subject.defense_die).to receive(:roll) { 1 }
+
+      subject.play!
+
+      expect(subject.winner).to eq(subject.player_2)
+    end
+
+    it 'plays 3 rounds which kills player_2' do
+      allow(subject.attack_die).to receive(:roll).and_return(11, 11, 11)
+      allow(subject.defense_die).to receive(:roll) { 1 }
+
+      subject.play!
+
+      expect(subject.winner).to eq(subject.player_1)
+    end
+
+  end
 end
