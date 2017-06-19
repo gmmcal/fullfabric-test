e7e5c77260d3744d8cabd80003026c2181422291
Leandro Maríngolo <leandronsp@gmail.com>
2017-06-19 15:08:39 +0100

    implementing round

--- lib/round.rb
+++ lib/round.rb
@@ -5,15 +5,16 @@ class Round
     :attack_die,
     :defense_die
 
-  def initialize( id: id,
-                  attacker: attacker,
-                  defender: defender,
-                  attack_die: attack_die,
-                  defense_die: defense_die)
+  def initialize(id:, attacker:, defender:, attack_die:, defense_die:)
     @id = id
     @attacker = attacker
     @defender = defender
     @attack_die = attack_die
     @defense_die = defense_die
+  end
+
+  def fight
+    attacker.attack(attack_die)
+    defender.defend(defense_die)
   end
 end
--- spec/round_spec.rb
+++ spec/round_spec.rb
@@ -2,25 +2,35 @@ require 'spec_helper'
 
 describe Round do
 
-  it 'takes 2 players, 2 dice' do
+  subject do
+    attacker = Player.new('attacker', 20)
+    defender = Player.new('defender', 20)
+    d1 = Die.new(10)
+    d2 = Die.new(8)
 
-    attacker = double('attacker')
-    defender = double('defender')
-    d1 = double('d1')
-    d2 = double('d2')
-
-    round = Round.new({
+    Round.new({
       id: 10,
       attacker: attacker,
       defender: defender,
       attack_die: d1,
       defense_die: d2
     })
-
-    expect(round.attacker).to_not be_nil
-    expect(round.defender).to_not be_nil
-    expect(round.attack_die).to_not be_nil
-    expect(round.defense_die).to_not be_nil
   end
 
+  it { expect(subject.attacker).to_not be_nil }
+  it { expect(subject.defender).to_not be_nil }
+  it { expect(subject.attack_die).to_not be_nil }
+  it { expect(subject.defense_die).to_not be_nil }
+
+  describe '.fight' do
+    it 'attacker attacks using attack die' do
+      expect(subject.attacker).to receive(:attack).with(subject.attack_die)
+      subject.fight
+    end
+
+    it 'defender defends using defense die' do
+      expect(subject.defender).to receive(:defend).with(subject.defense_die)
+      subject.fight
+    end
+  end
 end
